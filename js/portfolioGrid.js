86c6451d12f75cb49b56347ce2459b269b6b87a6
xmyoot <xmyoot@yahoo.com>
2017-08-10 17:47:59 -0700

    added a portfolio grid

---
+++ js/portfolioGrid.js
@@ -0,0 +1,33 @@
+//THESE FUNCTIONS CONTROL THE CONTENT ON THE PORTFOLIO GRID
+function displayAll(){
+  $('.flex-item').hide(1000);
+  $('.flex-item').show(1000);
+}
+function displayExp(){
+  $('.flex-item').hide(1000);
+  $('.experimentation').show(1000);
+}
+function displayProb(){
+  $('.flex-item').hide(1000);
+  $('.pd').show(1000);
+}
+function displayStat(){
+  $('.flex-item').hide(1000);
+  $('.statTests').show(1000);
+}
+function displayNLP(){
+  $('.flex-item').hide(1000);
+  $('.nlp').show(1000);
+}
+function displayDB(){
+  $('.flex-item').hide(1000);
+  $('.databases').show(1000);
+}
+function displayML(){
+  $('.flex-item').hide(1000);
+  $('.machineLearning').show(1000);
+}
+function displayBDA(){
+  $('.flex-item').hide(1000);
+  $('.bigData').show(1000);
+}
